a76dddd1ea9172c6eaa7c0a8bdd132c3109e0890
TobiasM95 <tobias.mielich@freenet.de>
2022-06-09 22:40:11 +0200

    Show icon in tooltip

--- GUI/src/LoadoutSolverWindow.cpp
+++ GUI/src/LoadoutSolverWindow.cpp
@@ -35,6 +35,9 @@ namespace TTM {
                 ImGui::PushFont(ImGui::GetCurrentContext()->IO.Fonts->Fonts[1]);
                 ImGui::Text(talent->getName().c_str());
                 ImGui::PopFont();
+                if (uiData.iconIndexMap.count(talent->index)) {
+                    ImGui::Image(uiData.iconIndexMap.at(talent->index).first.texture, ImVec2(uiData.treeEditorBaseTalentSize, uiData.treeEditorBaseTalentSize));
+                }
                 //ImGui::SameLine(ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize(idLabel.c_str()).x);
                 ImGui::Text(idLabel.c_str());
                 std::string talentTypeString;
@@ -66,6 +69,9 @@ namespace TTM {
                 ImGui::PushFont(ImGui::GetCurrentContext()->IO.Fonts->Fonts[1]);
                 ImGui::Text(talent->name.c_str());
                 ImGui::PopFont();
+                if (uiData.iconIndexMap.count(talent->index)) {
+                    ImGui::Image(uiData.iconIndexMap.at(talent->index).first.texture, ImVec2(uiData.treeEditorBaseTalentSize, uiData.treeEditorBaseTalentSize));
+                }
                 //ImGui::SameLine(ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize(idLabel.c_str()).x);
                 ImGui::Text(idLabel.c_str());
                 ImGui::TextColored(Presets::GET_TOOLTIP_TALENT_TYPE_COLOR(uiData.style), "(switch)");
@@ -89,6 +95,9 @@ namespace TTM {
                 ImGui::PushFont(ImGui::GetCurrentContext()->IO.Fonts->Fonts[1]);
                 ImGui::Text(talent->nameSwitch.c_str());
                 ImGui::PopFont();
+                if (uiData.iconIndexMap.count(talent->index)) {
+                    ImGui::Image(uiData.iconIndexMap.at(talent->index).second.texture, ImVec2(uiData.treeEditorBaseTalentSize, uiData.treeEditorBaseTalentSize));
+                }
                 //ImGui::SameLine(ImGui::GetContentRegionAvail().x - ImGui::CalcTextSize(idLabel.c_str()).x);
                 ImGui::Text(idLabel.c_str());
                 ImGui::TextColored(Presets::GET_TOOLTIP_TALENT_TYPE_COLOR(uiData.style), "(switch)");
