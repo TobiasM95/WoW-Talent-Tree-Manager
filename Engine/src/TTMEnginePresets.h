78b3689920423b078ec9c54eae54f348be1b2ff5
TobiasM95 <tobias.mielich@freenet.de>
2022-06-14 13:51:35 +0200

    menubar label color change, about version fix

--- Engine/src/TTMEnginePresets.h
+++ Engine/src/TTMEnginePresets.h
@@ -13,7 +13,7 @@ namespace Engine {
 }
 
 namespace Presets {//ENGINE PRESETS
-    static const std::string TTM_VERSION = "1.2.1";
+    static const std::string TTM_VERSION = "1.3.0";
 
     enum CLASS_IDS {
         CLASS_IDS_DEATHKNIGHT = 0,
