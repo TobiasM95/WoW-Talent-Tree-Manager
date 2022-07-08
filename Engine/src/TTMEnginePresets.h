c70807f629a671b2bb812fae39a27d2614221027
TobiasM95 <tobias.mielich@freenet.de>
2022-07-08 10:44:42 +0200

    update versions + changelog

--- Engine/src/TTMEnginePresets.h
+++ Engine/src/TTMEnginePresets.h
@@ -13,7 +13,7 @@ namespace Engine {
 }
 
 namespace Presets {//ENGINE PRESETS
-    static const std::string TTM_VERSION = "1.3.2";
+    static const std::string TTM_VERSION = "1.3.3";
 
     enum CLASS_IDS {
         CLASS_IDS_DEATHKNIGHT = 0,
