cf2a61f6ca6c29d26492d52d538442880227f7cd
TobiasM95 <tobias.mielich@freenet.de>
2022-07-13 21:44:39 +0200

    changed small ui detail, updated version to 1.3.4

--- Engine/src/TTMEnginePresets.h
+++ Engine/src/TTMEnginePresets.h
@@ -13,7 +13,7 @@ namespace Engine {
 }
 
 namespace Presets {//ENGINE PRESETS
-    static const std::string TTM_VERSION = "1.3.3";
+    static const std::string TTM_VERSION = "1.3.4";
 
     enum CLASS_IDS {
         CLASS_IDS_DEATHKNIGHT = 0,
