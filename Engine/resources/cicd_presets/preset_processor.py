7551a4b68a9eb7273c8f6a42ef67eb81478c8627
TobiasM95 <tobias.mielich@freenet.de>
2022-11-05 17:08:58 +0100

    added resource_versions.txt update routine to workflow

--- Engine/resources/cicd_presets/preset_processor.py
+++ Engine/resources/cicd_presets/preset_processor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import sys
 import hashlib
 import shutil
+from datetime import datetime
 
 
 # we always package all files together as a unit
@@ -17,6 +17,11 @@ target_copy_locations = [
     ],
 ]
 
+resource_version_locations = [
+    "../../../GUI/resources/resource_versions.txt",
+    "../../../GUI/resources/updatertarget/resource_versions.txt",
+]
+
 
 def main():
     current_hash = md5(current_files)
@@ -25,6 +30,7 @@ def main():
         for pfile, dfiles in zip(files_to_process, target_copy_locations):
             for dfile in dfiles:
                 shutil.copyfile(pfile, dfile)
+        update_resource_versions()
         return 1
     else:
         return 0
@@ -37,6 +43,23 @@ def md5(filepaths):
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+
+def update_resource_versions():
+    with open(resource_version_locations[0], "r") as orig_file:
+        orig_file_lines = orig_file.readlines()
+    version = orig_file_lines[0].split(";")[1]
+    current_date_string = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+    with open("./resource_versions.txt", "w") as new_file:
+        for line in orig_file_lines:
+            if line.startswith("presets"):
+                new_file.write(f"presets;{version};{current_date_string}\n")
+            elif line.startswith("nodeidorders"):
+                new_file.write(f"nodeidorders;{version};{current_date_string}\n")
+            else:
+                new_file.write(line)
+    for dfile in resource_version_locations:
+        shutil.copyfile("./resource_versions.txt", dfile)
 
 
 if __name__ == "__main__":
