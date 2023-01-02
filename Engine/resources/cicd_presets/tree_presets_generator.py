227a018ced42ebbb34988685fde8014017277249
TobiasM95 <tobias.mielich@freenet.de>
2023-01-02 13:47:35 +0100

    More path information output

--- Engine/resources/cicd_presets/tree_presets_generator.py
+++ Engine/resources/cicd_presets/tree_presets_generator.py
@@ -582,6 +582,7 @@ def md5(filepaths):
     hash_md5 = hashlib.md5()
     for filepath in filepaths:
         print(pathlib.Path(filepath).absolute().resolve())
+        print(pathlib.Path(filepath).stat().st_size)
         with open(filepath, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_md5.update(chunk)
