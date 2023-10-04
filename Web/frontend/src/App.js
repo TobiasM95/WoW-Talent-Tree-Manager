e94f685e08c67ae1d8d8c81aa2f1f26fc9fad810
TobiasM95 <tobias.mielich@freenet.de>
2023-10-04 23:14:41 +0200

    initial web app commit

---
+++ Web/frontend/src/App.js
@@ -0,0 +1,19 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
