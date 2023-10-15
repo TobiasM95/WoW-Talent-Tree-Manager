8c25a9a9711ca2848cb98210f4c7ab4cf3d18a28
TobiasM95 <tobias.mielich@freenet.de>
2023-10-16 00:34:33 +0200

    creation form and github stars

--- Web/frontend/src/scenes/login/index.jsx
+++ Web/frontend/src/scenes/login/index.jsx
@@ -1,34 +1,54 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Divider, TextField, Typography } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
-const initialValues = {
+// LOGIN SCHEMA
+
+const initialLoginValues = {
   userNameEmail: "",
   password: "",
 };
 
-const userSchema = yup.object().shape({
+const userLoginSchema = yup.object().shape({
   userNameEmail: yup.string().required("required"),
   password: yup.string().required("required"),
+});
+
+// CREATION SCHEMA
+
+const initialCreationValues = {
+  userName: "",
+  email: "",
+  initialPassword: "",
+};
+
+const userCreationSchema = yup.object().shape({
+  userName: yup.string().required("required"),
+  email: yup.string().email().required("required"),
+  initialPassword: yup.string().required("required"),
 });
 
 const Login = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const handleFormSubmit = (values) => {
+  const handleLoginFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  const handleCreationFormSubmit = (values) => {
     console.log(values);
   };
 
   return (
     <Box m="20px">
-      <Header title="LOGIN" subtitle="Login via password or SSO" />
+      <Header title="LOGIN" subtitle="Login via username and password" />
 
       <Formik
-        onSubmit={handleFormSubmit}
-        initialValues={initialValues}
-        validationSchema={userSchema}
+        onSubmit={handleLoginFormSubmit}
+        initialValues={initialLoginValues}
+        validationSchema={userLoginSchema}
       >
         {({
           values,
@@ -71,16 +91,94 @@ const Login = () => {
                 onBlur={handleBlur}
                 onChange={handleChange}
                 value={values.password}
-                name="Password"
+                name="password"
                 error={!!touched.password && !!errors.password}
                 helperText={touched.password && errors.password}
                 sx={{ gridColumn: "span 1" }}
               />
               {isNonMobile && <Box gridColumn="span 2" />}
             </Box>
-            <Box display="flex" justifyContent="center" mt="20px">
+            <Box display="flex" justifyContent="center" mt="20px" mb="20px">
               <Button type="submit" color="secondary" variant="contained">
                 Login
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+      <Divider />
+      <Formik
+        onSubmit={handleCreationFormSubmit}
+        initialValues={initialCreationValues}
+        validationSchema={userCreationSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(5, minmax(0, 1fr))"
+              mt="20px"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 5" },
+              }}
+            >
+              {isNonMobile && <Box gridColumn="span 2" />}
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Username"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.userName}
+                name="userName"
+                error={!!touched.userName && !!errors.userName}
+                helperText={touched.userName && errors.userName}
+                sx={{ gridColumn: "span 1" }}
+              />
+              {isNonMobile && <Box gridColumn="span 2" />}
+              {isNonMobile && <Box gridColumn="span 2" />}
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 1" }}
+              />
+              {isNonMobile && <Box gridColumn="span 2" />}
+              {isNonMobile && <Box gridColumn="span 2" />}
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.initialPassword}
+                name="initialPassword"
+                error={!!touched.initialPassword && !!errors.initialPassword}
+                helperText={touched.initialPassword && errors.initialPassword}
+                sx={{ gridColumn: "span 1" }}
+              />
+              {isNonMobile && <Box gridColumn="span 2" />}
+            </Box>
+            <Box display="flex" justifyContent="center" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create account
               </Button>
             </Box>
           </form>
