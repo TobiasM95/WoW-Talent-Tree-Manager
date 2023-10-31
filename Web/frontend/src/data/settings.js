033238a21d03eb83905bdb2b97dda6cc41a74657
TobiasM95 <tobias.mielich@freenet.de>
2023-10-31 20:01:55 +0100

    adjusted spacing settings

--- Web/frontend/src/data/settings.js
+++ Web/frontend/src/data/settings.js
@@ -1,7 +1,7 @@
 export const apiURL = "http://127.0.0.1:5000";
 export const treeViewerSettings = {
   nodeSize: 80,
-  gridSpace: 40,
+  gridSpace: 120,
 };
 export const treeNodeSettings = {
   imageFadeInDuration: 500,
