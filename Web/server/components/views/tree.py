fd17d9cc47f191a575138c9fa8d6bd87f3cec164
TobiasM95 <tobias.mielich@freenet.de>
2023-11-05 17:28:48 +0100

    added build endpoint and tweaked others

--- Web/server/components/views/tree.py
+++ Web/server/components/views/tree.py
@@ -9,8 +9,31 @@ from flask_jwt_extended import jwt_required, current_user
 @app.route("/tree/<string:content_id>", methods=["GET", "POST"])
 @jwt_required()
 def tree(content_id: str) -> Response:
+    if not db_handler.validate_content_access(current_user.user_id, content_id):
+        return (
+            jsonify(
+                {"success": False, "msg": "User does not have access to this content"}
+            ),
+            200,
+        )
     if request.method == "GET":
         return jsonify(get_tree_and_talent_descriptions(content_id)), 200
+    else:
+        return jsonify(content_id), 200
+
+
+@app.route("/build/<string:content_id>", methods=["GET", "POST"])
+@jwt_required()
+def build(content_id: str) -> Response:
+    if not db_handler.validate_content_access(current_user.user_id, content_id):
+        return (
+            jsonify(
+                {"success": False, "msg": "User does not have access to this content"}
+            ),
+            200,
+        )
+    if request.method == "GET":
+        return jsonify(get_build_information(content_id)), 200
     else:
         return jsonify(content_id), 200
 
@@ -62,3 +85,59 @@ def find_root_tree(content_id: str) -> tuple[Union[bool, None], Union[Tree, None
             )
 
     return is_imported, tree
+
+
+def get_build_information(content_id: str) -> dict:
+    is_imported, build, early_loadout = find_root_build(content_id)
+    build_info: dict[str, str] = {
+        "name": build.name,
+        "levelCap": build.level_cap,
+        "useLevelCap": build.use_level_cap,
+        "assignedSkills": build.assigned_skills,
+    }
+    tree_info: list[bool, dict] = get_tree_and_talent_descriptions(build.tree_id)
+    return {
+        "isImported": is_imported,
+        "classTalents": tree_info[1]["classTalents"],
+        "specTalents": tree_info[1]["specTalents"],
+        "loadoutName": "No loadout" if early_loadout is None else early_loadout.name,
+        "buildInformation": build_info,
+    }
+
+
+# Additionally, this returns the earliest loadout found in the import chain
+def find_root_build(content_id: str) -> tuple[bool, Build, Union[Loadout, None]]:
+    is_imported: bool = False
+    build: Build = db_handler.get_build(content_id)
+    early_loadout: Union[Loadout, None] = (
+        find_root_loadout(build.loadout_id)[1] if build.loadout_id is not None else None
+    )
+    while build.name is None:
+        is_imported = True
+        build: Build = db_handler.get_build(build.import_id)
+        if early_loadout is None:
+            early_loadout: Union[Loadout, None] = (
+                find_root_loadout(build.loadout_id)[1]
+                if build.loadout_id is not None
+                else None
+            )
+        if build.import_id is None and build.name is None:
+            raise Exception(
+                f"Import ID and name None at the same time {build.content_id}"
+            )
+
+    return is_imported, build, early_loadout
+
+
+def find_root_loadout(content_id: str) -> tuple[bool, Loadout]:
+    is_imported: bool = False
+    loadout: Loadout = db_handler.get_loadout(content_id)
+    while loadout.name is None:
+        is_imported = True
+        loadout: Loadout = db_handler.get_loadout(loadout.import_id)
+        if loadout.import_id is None and loadout.name is None:
+            raise Exception(
+                f"Import ID and name None at the same time {loadout.content_id}"
+            )
+
+    return is_imported, loadout
--- Web/server/components/views/workspace.py
+++ Web/server/components/views/workspace.py
@@ -3,7 +3,7 @@ from typing import Union
 from database_handler import Workspace, Tree, Loadout, Build
 from flask import jsonify
 from flask_jwt_extended import jwt_required, current_user
-from .tree import find_root_tree
+from .tree import find_root_tree, find_root_loadout, find_root_build
 
 
 @app.route("/workspace", methods=["GET"])
@@ -89,42 +89,3 @@ def get_workspace():
     }
 
     return jsonify(response_data), 200
-
-
-
-def find_root_loadout(content_id: str) -> tuple[bool, Loadout]:
-    is_imported: bool = False
-    loadout: Loadout = db_handler.get_loadout(content_id)
-    while loadout.name is None:
-        is_imported = True
-        loadout: Loadout = db_handler.get_loadout(loadout.import_id)
-        if loadout.import_id is None and loadout.name is None:
-            raise Exception(
-                f"Import ID and name None at the same time {loadout.content_id}"
-            )
-
-    return is_imported, loadout
-
-
-# Additionally, this returns the earliest loadout found in the import chain
-def find_root_build(content_id: str) -> tuple[bool, Build, Union[Loadout, None]]:
-    is_imported: bool = False
-    build: Build = db_handler.get_build(content_id)
-    early_loadout: Union[Loadout, None] = (
-        find_root_loadout(build.loadout_id)[1] if build.loadout_id is not None else None
-    )
-    while build.name is None:
-        is_imported = True
-        build: Build = db_handler.get_build(build.import_id)
-        if early_loadout is None:
-            early_loadout: Union[Loadout, None] = (
-                find_root_loadout(build.loadout_id)[1]
-                if build.loadout_id is not None
-                else None
-            )
-        if build.import_id is None and build.name is None:
-            raise Exception(
-                f"Import ID and name None at the same time {build.content_id}"
-            )
-
-    return is_imported, build, early_loadout
