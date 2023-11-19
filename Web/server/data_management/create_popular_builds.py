8a25f854a58e583760e873cdf0674056e238bcd7
TobiasM95 <tobias.mielich@freenet.de>
2023-11-19 13:48:45 +0100

    first auth to wcl api

--- Web/server/data_management/create_popular_builds.py
+++ Web/server/data_management/create_popular_builds.py
@@ -1,5 +1,43 @@
+import os
+from dotenv import load_dotenv
+import requests
+from requests_oauth2client import OAuth2Client, OAuth2ClientCredentialsAuth
+
+
 def main():
-    pass
+    load_dotenv()
+    oauth2client, oauth2token, oauth2auth, session = init_oauth2_session()
+    print(oauth2token)
+    test_body = """
+query {
+	worldData{
+		encounter(id: 2820){
+			characterRankings(
+				difficulty: 5,
+				className: "Druid",
+				specName: "Feral",
+			)
+		}
+	}
+}
+"""
+    res = session.get(
+        "https://www.warcraftlogs.com/api/v2/client", json={"query": test_body}
+    )
+
+
+def init_oauth2_session():
+    oauth2client = OAuth2Client(
+        token_endpoint="https://www.warcraftlogs.com/oauth/token",
+        client_id=os.environ["WCL_CLIENT_ID"],
+        client_secret=os.environ["WCL_CLIENT_SECRET"],
+    )
+    token = oauth2client.client_credentials()
+    auth = OAuth2ClientCredentialsAuth(oauth2client)
+    session = requests.Session()
+    session.auth = auth
+
+    return oauth2client, token, auth, session
 
 
 if __name__ == "__main__":
