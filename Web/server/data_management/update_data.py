bb5bbac90a00c9a63e4c7c0c6e82d565c9b4cd00
TobiasM95 <tobias.mielich@freenet.de>
2023-11-25 18:22:25 +0100

    more misc improvements

--- Web/server/data_management/update_data.py
+++ Web/server/data_management/update_data.py
@@ -1,13 +1,14 @@
 # NOTE:
 # This file starts all necessary data update tasks if necessary and should be run daily
+from data_management.create_popular_builds import create_popular_builds
 
 
 def main():
-    print("NYI")
     # Check if presets have been updated, if yes:
     ## run preset_updater
     ## run extract_icons_to_public
-    # in any case run create_popular_builds
+
+    create_popular_builds()
 
 
 if __name__ == "__main__":
