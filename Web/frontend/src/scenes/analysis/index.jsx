d7c6f9a5f0f9bf111a7ec167fbe32184839934e6
TobiasM95 <tobias.mielich@freenet.de>
2023-11-25 23:35:13 +0100

    prepared missing screens, removed debug screens and cleaned up

---
+++ Web/frontend/src/scenes/analysis/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Analysis = () => {
+  return (
+    <Box m="20px">
+      <Header title="Analysis" subtitle="Sim analysis coming soon!" />
+    </Box>
+  );
+};
+
+export default Analysis;
