e031526c0ff0999333602fe2d8512d9392a58972
TobiasM95 <tobias.mielich@freenet.de>
2023-11-28 22:39:28 +0100

    added preset tree selector to viewer entry page

--- Web/frontend/src/api/treeAPI.jsx
+++ Web/frontend/src/api/treeAPI.jsx
@@ -2,6 +2,7 @@ import { apiURL } from "../data/settings";
 
 const baseUrl = apiURL;
 const treeURL = baseUrl + "/tree";
+const presetTreeURL = treeURL + "/preset";
 
 function translateStatusToErrorMessage(status) {
   return "There was an error. Please try again.";
@@ -33,6 +34,19 @@ const treeAPI = {
       credentials: "include",
     };
     return fetch(`${treeURL}/${content_id}`, options)
+      .then(checkStatus)
+      .then(parseJSON)
+      .catch((error) => {
+        console.log("log client error " + error);
+        throw new Error("There was a client error during the login process.");
+      });
+  },
+  getPresetTreeContentID(className, specName) {
+    const options = {
+      method: "GET",
+      credentials: "include",
+    };
+    return fetch(`${presetTreeURL}/${className}/${specName}`, options)
       .then(checkStatus)
       .then(parseJSON)
       .catch((error) => {
