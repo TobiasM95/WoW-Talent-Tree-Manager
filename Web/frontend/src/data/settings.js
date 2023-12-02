6fedf57114b3beca5e398929e38510c63086db42
Tobias <tobi.mielich@gmail.com>
2023-12-02 16:58:49 +0100

    server setup

--- Web/frontend/src/data/settings.js
+++ Web/frontend/src/data/settings.js
@@ -1,4 +1,5 @@
-export const apiURL = "http://127.0.0.1:5000";
+//export const apiURL = "http://127.0.0.1:5000";
+export const apiURL = "https://chat-charlie.de/api";
 export const treeViewerSettings = {
   nodeSize: 80,
   gridSpace: 120,
