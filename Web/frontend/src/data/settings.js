6fa0db19db1c56f87060629dabcb725a9f3bf0cf
Tobias <tobi.mielich@gmail.com>
2023-12-02 23:02:05 +0100

    updated to new domain name

--- Web/frontend/src/data/settings.js
+++ Web/frontend/src/data/settings.js
@@ -1,5 +1,5 @@
 //export const apiURL = "http://127.0.0.1:5000";
-export const apiURL = "https://chat-charlie.de/api";
+export const apiURL = "https://talent-tree-manager.de/api";
 export const treeViewerSettings = {
   nodeSize: 80,
   gridSpace: 120,
